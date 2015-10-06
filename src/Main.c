a968f0c1e549af1d22a8bde6993e2c3c7b0573ef
Sam Beaven <darkhawke@gmail.com>
2015-10-06 19:11:12 +0100

    Tutorial part 2
    Added custom font and custom background

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -23,14 +26,24 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
 }
 
 static void main_window_load(Window *window) {
+  //Create Gfont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  
+  //Create GBitmap. This must be done before creating the textlayer
+  //As main_window_load draws things front to back
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
   //Create time textlayer
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorRed);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   
   //Improve layout
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   //Add it as a child layer
@@ -39,6 +52,9 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
   window_destroy(s_main_window);
 }
 

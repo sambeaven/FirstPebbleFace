3ba6450c5ade37d5a04da9dcd85494b73c2d8df2
Sam Beaven <darkhawke@gmail.com>
2015-10-13 13:15:36 +0100

    Added bluetooth connection status and battery mete

--- src/Main.c
+++ src/Main.c
@@ -1,11 +1,17 @@
 #include <pebble.h>
-
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
+  
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
-static BitmapLayer *s_background_layer;
-static GBitmap *s_background_bitmap;
+static BitmapLayer *s_background_layer, *s_bt_icon_layer;
+static GBitmap *s_background_bitmap, *s_bt_icon_bitmap;
 static TextLayer *s_date_layer;
+static TextLayer *s_weather_layer;
+static int s_battery_level;
+static Layer *s_battery_layer;
+
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -29,9 +35,33 @@ static void update_date(){
   text_layer_set_text(s_date_layer, buffer);
 }
 
+static void battery_update_proc(Layer *layer, GContext *ctx){
+  GRect bounds = layer_get_bounds(layer);
+  
+  int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   update_time();
   update_date();
+}
+
+static void battery_callback(BatteryChargeState state){
+  s_battery_level = state.charge_percent;
+  layer_mark_dirty(s_battery_layer);
+}
+
+static void bluetooth_callback(bool connected){
+  layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+  if (!connected){
+    vibes_double_pulse();
+  }
 }
 
 static void main_window_load(Window *window) {
@@ -50,35 +80,108 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   //Create date textlayer
-  s_date_layer = text_layer_create(GRect(5, 120, 144, 50));
+  s_date_layer = text_layer_create(GRect(5, 20, 144, 50));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_text(s_date_layer, "0101");
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  //Create temperature layer
+  s_weather_layer = text_layer_create(GRect(0, 130, 144, 25));
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_weather_layer, "Loading...");
   
-  //Improve layout
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  //Create battery meter layer
+  s_battery_layer = layer_create(GRect(14, 54, 115, 2));
+  layer_set_update_proc(s_battery_layer, battery_update_proc);
   
-  //Add it as a child layer
+  //Create bluetooth icon
+  s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_ICON);
+  s_bt_icon_layer = bitmap_layer_create(GRect(59, 12, 30, 30));
+  bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+  
+  //Add layers to window
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  layer_add_child(window_get_root_layer(window), s_battery_layer);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   fonts_unload_custom_font(s_time_font);
+  text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_weather_layer);
+  layer_destroy(s_battery_layer);
   gbitmap_destroy(s_background_bitmap);
   bitmap_layer_destroy(s_background_layer);
   window_destroy(s_main_window);
+  gbitmap_destroy(s_bt_icon_bitmap);
+  bitmap_layer_destroy(s_bt_icon_layer);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context){
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  Tuple *t = dict_read_first(iterator);
+  
+  while (t != NULL) {
+    switch(t->key) {
+      case KEY_TEMPERATURE:
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+        break;
+      case KEY_CONDITIONS:
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+        break;
+      default:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognised!", (int)t->key);
+        break;
+    }
+    
+    t = dict_read_next(iterator);
+  }
+  
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context){
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context){
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init() {
+  //register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+    
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-
+  battery_state_service_subscribe(battery_callback);
+  bluetooth_connection_service_subscribe(bluetooth_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  
   s_main_window = window_create();
   
   window_set_window_handlers(s_main_window, (WindowHandlers){
@@ -90,6 +193,10 @@ static void init() {
   
   update_time();
   update_date();
+  
+  // Ensure battery level is displayed from the start
+  battery_callback(battery_state_service_peek());
+  bluetooth_callback(bluetooth_connection_service_peek());
 }
 
 static void deinit() {
